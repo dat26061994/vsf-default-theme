2095bc10f138d4ad7a786c0e72e642e55ce38094
nthang 199 <hang75561@nuce.edu.vn>
2021-01-08 17:13:15 +0700

    home done

--- components/theme/blocks/Home/FeatureContent.vue
+++ components/theme/blocks/Home/FeatureContent.vue
@@ -2,13 +2,15 @@
 	<div
 		class="feature-content row container my-3 mx-auto px-md-5 px-sm-0 px-xs-0"
 	>
-		<div class="col-md-4 col-sm-12 mt-md-4 mt-sm-0 mt-xs-0 p-md-3 p-sm-2 p-2">
+		<div
+			class="col-md-4 col-sm-12 col-12 mt-md-4 mt-sm-0 mt-xs-0 p-md-3 p-sm-2 p-2"
+		>
 			<div class="row p-4 bg-feature-content">
 				<div
 					class="col-xs-3 d-flex justify-content-center align-items-center p-3"
 				>
 					<img
-						class="w-75"
+						class="image-home"
 						src="https://img.btdmp.com/files/10110793/2020/09/10/180x180@1599711359f32d671ca3.png"
 						alt=""
 					/>
@@ -27,7 +29,7 @@
 					class="col-xs-3 d-flex justify-content-center align-items-center p-3"
 				>
 					<img
-						class="w-75"
+						class="image-home"
 						src="https://img.btdmp.com/files/10110793/2020/09/10/180x180@1599711359f32d671ca3.png"
 						alt=""
 					/>
@@ -46,7 +48,7 @@
 					class="col-xs-3 d-flex justify-content-center align-items-center p-3"
 				>
 					<img
-						class="w-75"
+						class="image-home"
 						src="https://img.btdmp.com/files/10110793/2020/09/10/180x180@1599711359f32d671ca3.png"
 						alt=""
 					/>
@@ -65,5 +67,8 @@
 .bg-feature-content {
 	background-color: #ebf0ff;
 	color: #3e444b;
+}
+.image-home {
+	height: 2.5rem;
 }
 </style>
